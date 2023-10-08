0cde4ecdc2a81cc7a8cc6094bf2a53f3ce426687
gyeong deok <littleduck1219@gmail.com>
2023-10-08 23:15:30 +0900

    [Feat]Routing
    Routing 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<h1>Homepage</h1>} />
+			<Route path='/posts' element={<h1>postlist</h1>} />
+			<Route path='/posts/:id' element={<h1>post detail</h1>} />
+			<Route path='/posts/new' element={<h1>post new</h1>} />
+			<Route path='/posts/edit/:id' element={<h1>post edit</h1>} />
+			<Route path='/profile' element={<h1>profile</h1>} />
+			<Route path='/profile/edit' element={<h1>profile edit</h1>} />
+			<Route path='/notifications' element={<h1>notifications</h1>} />
+			<Route path='/search' element={<h1>search</h1>} />
+			<Route path='/signup' element={<h1>signup edit</h1>} />
+			<Route path='/login' element={<h1>login edit</h1>} />
+			<Route path='*' element={<Navigate replace to='/' />} />
+		</Routes>
+	);
+};
 
 export default App;
